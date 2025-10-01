ae9e21896a3e13e6a648b838a0b90a755d934d42
it24104180 <it24104180@my.sliit.lk>
2025-10-01 22:35:31 +0530

    add to lab 9

---
+++ Lab Sheet 09/it24104180.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\samod\OneDrive\\Desktop\\IT24104180\\Lab09")
+getwd
+
+# Set seed for reproducibility
+set.seed(123)
+
+# Part (i) Generate random sample
+sample_size <- 25
+mu <- 45
+sigma <- 2
+baking_times <- rnorm(sample_size, mean = mu, sd = sigma)
+print(baking_times)
+
+# Part (ii) Hypothesis test
+# H0: mean = 46
+# H1: mean < 46
+
+t_test_result <- t.test(baking_times, mu = 46, alternative = "less")
+print(t_test_result)
